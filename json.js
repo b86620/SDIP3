48b37e7f7ae692f284be2bb77b9df9aa17392a85
Anthony Arrants <antnya@fullsail.edu>
2014-07-24 22:44:07 -0400

    updated JSON.

--- json.js
+++ json.js
@@ -3,20 +3,37 @@
 // Anthony Arrants
 // SDI 1407
 
-var JSONStr = '{"People":[\
-            {"Person1":[\
-                {"Name":"Bob"}\
-                {"Age":10}\
-                ]\
-            }\
-            {"Person2":[\
-                {"Name":"Joe"}\
-                {"Age":20}\
-                ]\
-            }\
-            {"Person3": [\
-                {"Name":"Emily"}\
-                {"Age":26}\
-                ]\
-            }\
-        ]}';
+var JSONStr = {
+    "People": [
+ 	{
+            "Person1": [
+                {
+                    "Name": "Bob"
+                },
+                {
+                    "Age": 10
+                }
+            ]
+ 	},
+ 	{
+            "Person2": [
+                {
+                    "Name": "Joe"
+                },
+                {
+                    "Age": 20
+                }
+            ]
+        },
+        {
+            "Person3": [
+                {
+                    "Name": "Emily"
+                },
+                {
+                    "Age": 26
+                }
+            ]
+ 	}
+    ]
+};
